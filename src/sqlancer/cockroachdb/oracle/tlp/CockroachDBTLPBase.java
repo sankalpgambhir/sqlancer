aa6b76186b11e68a8339584721c1c4edc6ccf709
Sankalp Gambhir <sankalp.gambhir47@gmail.com>
2023-01-05 13:32:49 +0100

    CockroachDB helpers

--- src/sqlancer/cockroachdb/oracle/tlp/CockroachDBTLPBase.java
+++ src/sqlancer/cockroachdb/oracle/tlp/CockroachDBTLPBase.java
@@ -12,10 +12,16 @@ import sqlancer.cockroachdb.CockroachDBSchema;
 import sqlancer.cockroachdb.CockroachDBSchema.CockroachDBColumn;
 import sqlancer.cockroachdb.CockroachDBSchema.CockroachDBTable;
 import sqlancer.cockroachdb.CockroachDBSchema.CockroachDBTables;
+import sqlancer.cockroachdb.ast.CockroachDBBetweenOperation;
+import sqlancer.cockroachdb.ast.CockroachDBBinaryComparisonOperator;
+import sqlancer.cockroachdb.ast.CockroachDBBinaryLogicalOperation;
 import sqlancer.cockroachdb.ast.CockroachDBColumnReference;
 import sqlancer.cockroachdb.ast.CockroachDBExpression;
+import sqlancer.cockroachdb.ast.CockroachDBNotOperation;
 import sqlancer.cockroachdb.ast.CockroachDBSelect;
 import sqlancer.cockroachdb.ast.CockroachDBTableReference;
+import sqlancer.cockroachdb.ast.CockroachDBBinaryComparisonOperator.CockroachDBComparisonOperator;
+import sqlancer.cockroachdb.ast.CockroachDBBinaryLogicalOperation.CockroachDBBinaryLogicalOperator;
 import sqlancer.cockroachdb.gen.CockroachDBExpressionGenerator;
 import sqlancer.cockroachdb.oracle.CockroachDBNoRECOracle;
 import sqlancer.common.gen.ExpressionGenerator;
@@ -66,6 +72,184 @@ public class CockroachDBTLPBase extends
     @Override
     protected ExpressionGenerator<CockroachDBExpression> getGen() {
         return gen;
+    }
+
+
+
+    public CockroachDBExpression genSplitPredicate(CockroachDBExpression pred){
+        if(pred instanceof CockroachDBBetweenOperation){
+            if(Randomly.getBoolean()){
+                // split
+                CockroachDBBetweenOperation betweenPred = (CockroachDBBetweenOperation) pred;
+                
+                switch (betweenPred.getType()) {
+                    case BETWEEN:
+                        /**
+                         * b between a and c -->
+                         *      a <= b and b <= c
+                         */
+                        return new CockroachDBBinaryLogicalOperation(
+                            new CockroachDBBinaryComparisonOperator(betweenPred.getLeft(), betweenPred.getExpr(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                            new CockroachDBBinaryComparisonOperator(betweenPred.getExpr(), betweenPred.getRight(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                            CockroachDBBinaryLogicalOperator.AND);
+                    case NOT_BETWEEN:
+                        /**
+                         * b not between a and c -->
+                         *      not (a <= b and b <= c)
+                         *      a > b or b > c
+                         * with equal probability
+                         */
+                        if(Randomly.getBoolean()){
+                            return new CockroachDBNotOperation(
+                                new CockroachDBBinaryLogicalOperation(
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getLeft(), betweenPred.getExpr(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getExpr(), betweenPred.getRight(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                    CockroachDBBinaryLogicalOperator.AND)
+                                );
+                        }
+                        else{
+                            return new CockroachDBBinaryLogicalOperation(
+                            new CockroachDBBinaryComparisonOperator(betweenPred.getLeft(), betweenPred.getExpr(), CockroachDBComparisonOperator.GREATER), 
+                            new CockroachDBBinaryComparisonOperator(betweenPred.getExpr(), betweenPred.getRight(), CockroachDBComparisonOperator.GREATER), 
+                            CockroachDBBinaryLogicalOperator.OR);
+                        }
+                    case BETWEEN_SYMMETRIC:
+                        /**
+                         * b between symmetric a and c -->
+                         *      (a <= b and b <= c) or (c <= b and b <= a)
+                         */
+                        return new CockroachDBBinaryLogicalOperation(
+                            new CockroachDBBinaryLogicalOperation(
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getLeft(), betweenPred.getExpr(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getExpr(), betweenPred.getRight(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                    CockroachDBBinaryLogicalOperator.AND), 
+                                new CockroachDBBinaryLogicalOperation(
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getRight(), betweenPred.getExpr(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getExpr(), betweenPred.getLeft(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                    CockroachDBBinaryLogicalOperator.AND),
+                            CockroachDBBinaryLogicalOperator.OR);
+                    case NOT_BETWEEN_SYMMETRIC:
+                        /**
+                         * b not between symmetric a and c -->
+                         *      not ((a <= b and b <= c) or (c <= b and b <= a))
+                         *      (b < a and b < c) or (b > a and b > c)
+                         * with equal probability
+                         */
+                        if(Randomly.getBoolean()){
+                            return new CockroachDBNotOperation(
+                                new CockroachDBBinaryLogicalOperation(
+                                    new CockroachDBBinaryLogicalOperation(
+                                            new CockroachDBBinaryComparisonOperator(betweenPred.getLeft(), betweenPred.getExpr(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                            new CockroachDBBinaryComparisonOperator(betweenPred.getExpr(), betweenPred.getRight(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                            CockroachDBBinaryLogicalOperator.AND), 
+                                        new CockroachDBBinaryLogicalOperation(
+                                            new CockroachDBBinaryComparisonOperator(betweenPred.getRight(), betweenPred.getExpr(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                            new CockroachDBBinaryComparisonOperator(betweenPred.getExpr(), betweenPred.getLeft(), CockroachDBComparisonOperator.SMALLER_EQUALS), 
+                                            CockroachDBBinaryLogicalOperator.AND),
+                                    CockroachDBBinaryLogicalOperator.OR)
+                                );
+                        }
+                        else{
+                            return new CockroachDBBinaryLogicalOperation(
+                                new CockroachDBBinaryLogicalOperation(
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getLeft(), betweenPred.getExpr(), CockroachDBComparisonOperator.SMALLER), 
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getRight(), betweenPred.getExpr(), CockroachDBComparisonOperator.SMALLER), 
+                                    CockroachDBBinaryLogicalOperator.AND), 
+                                new CockroachDBBinaryLogicalOperation(
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getLeft(), betweenPred.getExpr(), CockroachDBComparisonOperator.GREATER), 
+                                    new CockroachDBBinaryComparisonOperator(betweenPred.getRight(), betweenPred.getExpr(), CockroachDBComparisonOperator.GREATER), 
+                                    CockroachDBBinaryLogicalOperator.AND), 
+                            CockroachDBBinaryLogicalOperator.OR);
+                        }
+        
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // TODO: make recurse to find betweens? possibly not much added benefit
+        return pred;
+    }
+
+    public CockroachDBExpression genBetweenDiscardedPredicate(CockroachDBExpression pred, Boolean right){
+        if(pred instanceof CockroachDBBetweenOperation){
+            if(Randomly.getBoolean()){
+                // split
+                CockroachDBBetweenOperation betweenPred = (CockroachDBBetweenOperation) pred;
+                
+                switch (betweenPred.getType()) {
+                    case BETWEEN:
+                        /**
+                         * b between a and c -->
+                         *      a <= b if not right
+                         *      b <= c if right
+                         * 
+                         */
+                        return new CockroachDBBinaryComparisonOperator(
+                            right ? betweenPred.getLeft() : betweenPred.getExpr(), 
+                            right ? betweenPred.getExpr() : betweenPred.getRight(),
+                            CockroachDBComparisonOperator.SMALLER_EQUALS);
+
+                    // case NOT_BETWEEN:
+                        
+                    // case BETWEEN_SYMMETRIC:
+                        
+                    // case NOT_BETWEEN_SYMMETRIC:
+                        
+        
+                    default:
+                        break;
+                }
+            }
+        }
+
+        // recurse
+        if(pred instanceof CockroachDBBinaryLogicalOperation){
+            if(Randomly.getBoolean()){
+                // split
+                CockroachDBBinaryLogicalOperation binaryPred = (CockroachDBBinaryLogicalOperation) pred;
+
+                return new CockroachDBBinaryLogicalOperation(
+                    genBetweenDiscardedPredicate(binaryPred.getLeft(), right), 
+                    genBetweenDiscardedPredicate(binaryPred.getRight(), right), 
+                    binaryPred.getOp());
+            }
+        }
+
+        // else
+        return pred;
+    }
+
+    public CockroachDBExpression genSplitComparisonPredicate(CockroachDBExpression pred){
+        if(pred instanceof CockroachDBBinaryComparisonOperator){
+            CockroachDBBinaryComparisonOperator binaryPred = (CockroachDBBinaryComparisonOperator) pred;
+            
+            if(
+                binaryPred.getOp() != CockroachDBComparisonOperator.GREATER_EQUALS ||
+                binaryPred.getOp() != CockroachDBComparisonOperator.SMALLER_EQUALS
+            ){
+                return new CockroachDBBinaryComparisonOperator(
+                genSplitComparisonPredicate(binaryPred.getLeft()),
+                genSplitComparisonPredicate(binaryPred.getRight()), 
+                binaryPred.getOp());
+            }
+            else{
+                // rewrite
+                CockroachDBExpression newLeft = genSplitComparisonPredicate(binaryPred.getLeft());
+                CockroachDBExpression newRight = genSplitComparisonPredicate(binaryPred.getRight());
+
+                // l <= r --> l < r OR l == r
+                // l >= r --> l > r OR l == r
+                return new CockroachDBBinaryLogicalOperation(
+                new CockroachDBBinaryComparisonOperator(newLeft, newRight, binaryPred.getOp() == CockroachDBComparisonOperator.GREATER_EQUALS ? CockroachDBComparisonOperator.GREATER : CockroachDBComparisonOperator.SMALLER),
+                new CockroachDBBinaryComparisonOperator(newLeft, newRight, CockroachDBComparisonOperator.EQUALS),
+                CockroachDBBinaryLogicalOperator.OR);
+            }
+        }
+
+        // else, unimplemented recursion
+        return pred;
     }
 
 }
